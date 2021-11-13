cccf896b04071894b25915d53c73b5309a8eb71a
James <pitorjames@gmail.com>
2021-11-13 14:40:40 +1000

    Added html front end, year.run() now returns string instead of print statement

---
+++ backend.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import numpy as np
+
+
+class Year:
+
+    def __init__(self, y):
+        self.year = y
+        self.population = None
+        self.population_dataset = pd.read_csv("Projected population, Australia.csv", header=1)
+        self.powerStation_dataset = None
+        self.population_dataset = self.population_dataset.rename(columns={'Unnamed: 0': "Year"})
+        self.powerStation_dataset = pd.read_csv("MajorPowerStations_v2.csv", header=0, index_col=0)
+        self.plant_types = self.powerStation_dataset['PRIMARYFUELTYPE'].unique()
+        self.plant_types = np.delete(self.plant_types, np.where(self.plant_types == ["Diesel"]))
+
+    def NumberOfPowerplants(self):
+        string = ''
+        for plant in self.plant_types:
+            arr = []
+
+            for row in self.powerStation_dataset.iterrows():
+                if row[1][8] == plant:
+                    arr.append(row[1][10])
+
+            average = ((round(round(sum(arr)) / len(arr))) * 1000) * 8765
+            string = string + "<br/>     {number} {type} plants".format(number=round(self.CalculateKW() / average),
+                                                                     type=plant)
+        return string
+
+    def CalculatePop(self):
+        yearIndex = self.population_dataset[self.population_dataset['Year'] == self.year].index.values
+        pop = self.population_dataset["Medium series"][yearIndex].tolist()[0]
+        pop = pop.replace(',', '')
+        self.population = int(pop)
+
+    def CalculateKW(self):
+        return 8930 * self.population
+
+    def run(self):
+        self.CalculatePop()
+        kilowatt = self.CalculateKW()
+
+        return "By {year} Australia will have a projected population of {pop}, to support this population Australia " \
+               "will have to produce at least {kw} Billion Kwh per year. <br/>This is equivalent to:".format(
+                year=self.year, pop=self.population, kw=kilowatt / 1000000000) + self.NumberOfPowerplants()
+
+
